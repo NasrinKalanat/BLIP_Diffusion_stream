041338abcf57a7ff331846a86c0a68e08efd20eb
NasrinKalanat <NAK168@pitt.edu>
2024-09-23 15:21:17 -0400

    bug fixed

--- inference_generation.py
+++ inference_generation.py
@@ -1,7 +1,10 @@
+import os.path
+
 import torch
 from torchvision.utils import save_image
 from lavis.models import load_model_and_preprocess
 import argparse
+import glob
 
 parser = argparse.ArgumentParser(
                     prog='BLIP-Diffusion')
@@ -13,39 +16,45 @@ finetuned_ckpt = args.ckpt_path
 
 model.load_checkpoint(finetuned_ckpt)
 
-cond_subject = "scene"
-tgt_subject = "scene"
-text_prompt = "raining"
-# prompt = "in oil painting"
+all_files = glob.glob("images/*.png")
 
-cond_subjects = [txt_preprocess["eval"](cond_subject)]
-tgt_subjects = [txt_preprocess["eval"](tgt_subject)]
-text_prompt = [txt_preprocess["eval"](text_prompt)]
+prompts = []
+for file in all_files:
+    _, idx, _, text_prompt = file.split("_")
+    cond_subject = "scene"
+    tgt_subject = "scene"
+    # text_prompt = "snowy"
 
-samples = {
-    "cond_images": None,
-    "cond_subject": cond_subjects,
-    "tgt_subject": tgt_subjects,
-    "prompt": text_prompt,
-}
+    cond_subjects = [txt_preprocess["eval"](cond_subject)]
+    tgt_subjects = [txt_preprocess["eval"](tgt_subject)]
+    text_prompt = [txt_preprocess["eval"](text_prompt)]
 
-num_output = 4
+    samples = {
+        "cond_images": None,
+        "cond_subject": cond_subjects,
+        "tgt_subject": tgt_subjects,
+        "prompt": text_prompt,
+    }
 
-iter_seed = 8888
-guidance_scale = 7.5
-num_inference_steps = 50
-negative_prompt = "over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
+    num_output = 4
 
-for i in range(num_output):
-    output = model.generate(
-        samples,
-        seed=iter_seed + i,
-        guidance_scale=guidance_scale,
-        num_inference_steps=num_inference_steps,
-        neg_prompt=negative_prompt,
-        height=512,
-        width=512,
-    )
+    iter_seed = 8888
+    guidance_scale = 7.5
+    num_inference_steps = 50
+    negative_prompt = "over-exposure, under-exposure, saturated, duplicate, out of frame, lowres, cropped, worst quality, low quality, jpeg artifacts, morbid, mutilated, out of frame, ugly, bad anatomy, bad proportions, deformed, blurry, duplicate"
 
-    output[0].save(f'generated_{cond_subject}_{tgt_subject}_{i}.png')
+    for i in range(num_output):
+        output = model.generate(
+            samples,
+            seed=iter_seed + i,
+            guidance_scale=guidance_scale,
+            num_inference_steps=num_inference_steps,
+            neg_prompt=negative_prompt,
+            height=512,
+            width=512,
+        )
+
+        if not os.path.exists("output"):
+            os.mkdir("output")
+        output[0].save(f'output/generated_{idx}_{cond_subject}_{tgt_subject}_{text_prompt}.png')
 
