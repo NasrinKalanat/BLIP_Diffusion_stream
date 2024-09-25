c88ea27ee29af2cbbe130254beec2c79ba8ff48d
NasrinKalanat <NAK168@pitt.edu>
2024-09-24 21:17:51 -0400

    bugs fixed

--- download_images.py
+++ download_images.py
@@ -132,7 +132,7 @@ def load_data(path, path_weather, fol, phase="train"):
 
 # download images (do just for first time)
 images_path = "images"
-imgs = load_data(path= "/data/nak168/spatial_temporal/stream_img/data/fpe-westbrook/", path_weather= "/data/nak168/spatial_temporal/stream_img/data/", fol= "West Brook Upper_01171030")
+imgs = load_data(path= "/data/nak168/spatial_temporal/stream_img/data/fpe-westbrook/", path_weather= "/data/nak168/spatial_temporal/stream_img/data/", fol= "West Whately_01171005")
 if not os.path.exists(images_path):
   os.mkdir(images_path)
 delay=0.001
